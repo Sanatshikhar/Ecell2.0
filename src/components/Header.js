80d621bc9ef99e77d251621c9de0a7c886b02a1a
Sanat Shikhar Sinha <shikharsanat@gmail.com>
2025-08-24 21:00:09 +0530

    fix join button

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,40 +14,40 @@ const Header = () => {
   return (
     <div>
   <div className="fixed bg-black bg-opacity-20 backdrop-blur-md rounded-3xl w-[80%] md:w-[90%] lg:w-[80%] top-6 left-1/2 -translate-x-1/2 h-[60px] md:h-[70px] lg:h-[80px] flex items-center z-50 box-border transition-none px-4 md:px-8 lg:px-12">
-        {/* Logo */}
-        <div className="flex items-center h-full flex-shrink-0">
-          <img src={logo} alt="Logo" className="w-[60px] md:w-[80px] lg:w-[100px] max-h-[50px] md:max-h-[60px] lg:max-h-[80px] object-contain" />
-        </div>
+    {/* Logo */}
+    <div className="flex items-center h-full flex-shrink-0">
+      <img src={logo} alt="Logo" className="w-[60px] md:w-[80px] lg:w-[100px] max-h-[50px] md:max-h-[60px] lg:max-h-[80px] object-contain" />
+    </div>
 
-        {/* Menu Links (Tablet/Laptop/Big Screens) */}
-        <div className="hidden md:flex items-center space-x-14 md:space-x-10 absolute left-1/2 -translate-x-1/2 top-1/2 text-white font-semibold -translate-y-1/2 z-20 h-[40px] ">
-          <NavLink to="/" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''} end>
-            Home
-          </NavLink>
-          <NavLink to="/team" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
-            Team
-          </NavLink>
-          <NavLink to="/gallery" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
-            Gallery
-          </NavLink>
-          <NavLink to="/about" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
-            About
-          </NavLink>
-          <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
-            Contact
-          </NavLink>
-        </div>
+    {/* Menu Links (Tablet/Laptop/Big Screens) */}
+    <div className="hidden md:flex items-center space-x-14 md:space-x-10 absolute left-1/2 -translate-x-1/2 top-1/2 text-white font-semibold -translate-y-1/2 z-20 h-[40px] ">
+      <NavLink to="/" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''} end>
+        Home
+      </NavLink>
+      <NavLink to="/team" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
+        Team
+      </NavLink>
+      <NavLink to="/gallery" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
+        Gallery
+      </NavLink>
+      <NavLink to="/about" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
+        About
+      </NavLink>
+      <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-[#ab49f6] underline underline-offset-8 font-bold' : ''}>
+        Contact
+      </NavLink>
+    </div>
 
-        {/* Join Us Button (Tablet/Laptop/Big Screens) */}
-        <div className="flex-shrink-0 flex items-center">
-          <Link
-            to="/members"
-            className="hidden md:flex bg-white h-[45px] w-[110px] md:w-[130px] lg:w-[150px] rounded-3xl text-black font-semibold text-center justify-center px-4 md:px-6 lg:px-8 items-center transition hover:bg-black hover:text-white hover:scale-105 border-white border-[1px] shadow-md focus:outline-none focus:ring-2 focus:ring-[#a259ff]"
-            style={{fontSize:'1rem',letterSpacing:'0.02em',boxSizing:'border-box'}}
-          >
-            Join Us
-          </Link>
-        </div>
+    {/* Join Us Button (Tablet/Laptop/Big Screens) - right aligned */}
+    <div className="hidden md:flex flex-shrink-0 items-center ml-auto">
+      <Link
+        to="/members"
+        className="bg-white h-[45px] w-[110px] md:w-[130px] lg:w-[150px] rounded-3xl text-black font-semibold text-center justify-center px-4 md:px-6 lg:px-8 items-center transition hover:bg-black hover:text-white hover:scale-105 border-white border-[1px] shadow-md focus:outline-none focus:ring-2 focus:ring-[#a259ff] flex"
+        style={{fontSize:'1rem',letterSpacing:'0.02em',boxSizing:'border-box'}}
+      >
+        Join Us
+      </Link>
+    </div>
 
         {/* Hamburger/Cross Button */}
         <button
